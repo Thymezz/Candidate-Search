00338845e847ef0b16fd8a775a0b5fec93f4651e
Thomas Vogel <Lenex5109@yahoo.com>
2025-02-20 18:25:02 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  base:'./',
   preview: {
     port: parseInt(process.env.PORT || '4173'), // Use Render's PORT
     host: true, // Allow external access
